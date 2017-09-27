47f5be0e2283b57f04898755141068d1e8f720df
Maciej Gruszka <maciej.gruszka@oracle.com>
2017-09-27 09:51:41 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+WLST="/u01/oracle/oracle_common/common/bin/wlst.sh -skipWLSModuleScanning"
+
+echo "Hello from the WebLogic Step"
+echo "Reading the config from :" $WEBLOGIC_STEP_CONFIG
+cat $WEBLOGIC_STEP_CONFIG
+
+echo "Create domains directory"
+mkdir -p /u01/oracle/domains
+
+echo "Create applications directory"
+mkdir -p /u01/oracle/applications
+
+echo "Checking we can run wlst"
+$WLST $WERCKER_SOURCE_DIR/test.py
+
+echo "Creating the basic domain"
+$WLST $WERCKER_SOURCE_DIR/create-domain.py
+
+echo "Create data sources"
+$WLST $WERCKER_SOURCE_DIR/create-data-sources.py $WEBLOGIC_STEP_CONFIG
+
+echo "Deploy applications"
+$WLST $WERCKER_SOURCE_DIR/deploy-applications.py $WEBLOGIC_STEP_CONFIG
